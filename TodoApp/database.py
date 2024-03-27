8d8cb2e283e74278e95ccabaef4d6ec4942ebe1a
alfonWaz13 <alfonsoramirez136@gmail.com>
2024-03-27 09:14:00 +0100

    feat(database): add get_database function

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -7,3 +7,11 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_database():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
