d0bd9802a4c6a8f46d9a9efe7224f09007869822
alfonWaz13 <alfonsoramirez136@gmail.com>
2024-03-27 09:16:26 +0100

    refactor(database): move all database related content to database module

--- TodoApp/application.py
+++ TodoApp/application.py
@@ -1,12 +1,21 @@
+from typing import Annotated
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from database import engine
-import models
-
+from TodoApp import models
+from TodoApp.models import ToDos
+from TodoApp.database import get_database, engine
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_database)]):
+    return db.query(ToDos).all()
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
