00cf6d0710349fc60afb79705a54c21d57197e94
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-01 09:07:34 +0200

    feat(application): add update_todo endpoint

--- TodoApp/application.py
+++ TodoApp/application.py
@@ -36,5 +36,20 @@ async def create_todo(db: db_dependency, todo_request: ToDoRequest):
     db.commit()
 
 
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_id: int, todo_request: ToDoRequest):
+    todo_to_update: ToDos = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo_to_update is None:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+    todo_to_update.title = todo_request.title
+    todo_to_update.description = todo_request.description
+    todo_to_update.priority = todo_request.priority
+    todo_to_update.complete = todo_request.complete
+
+    db.add(todo_to_update)
+    db.commit()
+
+
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
