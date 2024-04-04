1cd8de67a19445ab68c35239ed4f241a4151693f
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-04 09:30:55 +0200

    feat(create-user): add create_user endpoint

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,28 @@
 from fastapi import APIRouter
+from passlib.context import CryptContext
+from starlette import status
+
+from TodoApp.database import db_dependency
+from TodoApp.models import Users
+from TodoApp.schemas import CreateUserRequest
+
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, request: CreateUserRequest):
+    user_to_create = Users(
+        email=request.email,
+        username=request.username,
+        first_name=request.first_name,
+        last_name=request.last_name,
+        role=request.role,
+        hashed_password=bcrypt_context.hash(request.password),
+        is_active=True
+    )
+
+    db.add(user_to_create)
+    db.commit()
