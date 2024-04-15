b4a6d46a54c3e6b9360b2143f6f8a46d1d340222
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-15 08:54:02 +0200

    feat(authenticate-user): add login_for_access_token endpoint

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,15 +1,23 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from passlib.context import CryptContext
 from starlette import status
 
 from TodoApp.database import db_dependency
 from TodoApp.models import Users
 from TodoApp.schemas import CreateUserRequest
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter()
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    return user is not None and bcrypt_context.verify(password, user.hashed_password)
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
@@ -26,3 +34,11 @@ async def create_user(db: db_dependency, request: CreateUserRequest):
 
     db.add(user_to_create)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Success Authentication'
