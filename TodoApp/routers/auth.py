3050ceed02b943bb5d12f64de2921de63b4b1ba5
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-17 08:59:40 +0200

    feat(authenticate-user): update login_for_access_token to return both access_token and token_type using Token model

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -6,7 +6,7 @@ from passlib.context import CryptContext
 from starlette import status
 
 from TodoApp.database import db_dependency
-from TodoApp.models import Users
+from TodoApp.models import Users, Token
 from TodoApp.schemas import CreateUserRequest
 from fastapi.security import OAuth2PasswordRequestForm
 from jose import jwt
@@ -49,11 +49,11 @@ async def create_user(db: db_dependency, request: CreateUserRequest):
     db.commit()
 
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return 'Failed Authentication'
 
     token = create_access_token(user.username, user.id, timedelta(minutes=TOKEN_EXPIRATION_TIME_MINUTES))
-    return token
+    return {'access_token': token, 'token_type': 'bearer'}
