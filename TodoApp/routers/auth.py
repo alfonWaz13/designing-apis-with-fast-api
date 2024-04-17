7f6db8b87f745905ea58f06d7d88452cfab63d99
alfonWaz13 <alfonwaz13@icloud.com>
2024-04-17 09:15:37 +0200

    refactor(authenticate-user): update login_for_access_token to raise HTTP exception

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -67,7 +67,7 @@ async def create_user(db: db_dependency, request: CreateUserRequest):
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return 'Failed Authentication'
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
 
     token = create_access_token(user.username, user.id, timedelta(minutes=TOKEN_EXPIRATION_TIME_MINUTES))
     return {'access_token': token, 'token_type': 'bearer'}
