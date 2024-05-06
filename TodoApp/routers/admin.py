611e36c4fec40d1a162d7114b81c7f9e1d595a69
alfonWaz13 <alfonwaz13@icloud.com>
2024-05-06 09:03:20 +0200

    feat(admin-role): add delete_todo endpoint

--- TodoApp/routers/admin.py
+++ TodoApp/routers/admin.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 from starlette import status
 
 from TodoApp.database import db_dependency
@@ -10,6 +10,20 @@ router = APIRouter(prefix='/admin', tags=['admin'])
 
 @router.get("/todo", status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
-    if user.get('role') != 'admin':
-        raise HTTPException(status_code=401, detail='Authentication failed')
+    await check_that_user_is_admin(user)
     return db.query(ToDos).all()
+
+
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    await check_that_user_is_admin(user)
+    todo_to_delete = db.query(ToDos).filter(ToDos.id == todo_id)
+    if todo_to_delete.first() is None:
+        raise HTTPException(status_code=404, detail='Todo not found.')
+    todo_to_delete.delete()
+    db.commit()
+
+
+async def check_that_user_is_admin(user):
+    if user.get('role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
