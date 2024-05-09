9c6a39bb9e59d7488154c0dbd74bebb6ce848099
alfonWaz13 <alfonwaz13@icloud.com>
2024-05-09 10:00:21 +0200

    feat(users): add change_password method

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -14,6 +14,18 @@ async def get_user(user: user_dependency, db: db_dependency):
 
     if user is None:
         raise HTTPException(status_code=404, detail='User not found.')
-    return user_info
     return db.query(Users).filter(Users.id == user.get('id')).first()
 
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency, db: db_dependency, new_password_request: NewPasswordRequest):
+
+    if user is None:
+        raise HTTPException(status_code=404, detail='User not found.')
+
+    user_to_update = db.query(Users).filter(Users.id == user.get('id')).first()
+    if not bcrypt_context.verify(new_password_request.current_password, user_to_update.hashed_password):
+        raise HTTPException(status_code=401, detail='Error on password update.')
+    user_to_update.hashed_password = bcrypt_context.hash(new_password_request.new_password)
+    db.add(user_to_update)
+    db.commit()
