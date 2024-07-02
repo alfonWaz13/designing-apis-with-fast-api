48da2e7631dc7604dbcce63d30633dc6c99e3f0c
alfonWaz13 <alfonwaz13@icloud.com>
2024-07-02 09:49:23 +0200

    feat(database): update database connection to use PostgreSQL

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+DATABASE_SQL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(DATABASE_SQL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
